b4f0574620d9612644659908a1f0d45a95b0bb6e
jumbo ken <subsubmailer@gmail.com>
2014-08-31 22:40:29 +0900

    できたぞ

--- sleeper.js
+++ sleeper.js
@@ -3,17 +3,13 @@ Bleacon.startScanning();
 
 var exec = require('child_process').exec;
 
+// get iBeacon
 Bleacon.on('discover', function(bleacon) {
 	   console.dir(bleacon);
-var child = exec('pmset displaysleepnow', function(err, stdout, stderr) {
-  if (!err) {
-      console.log('stdout: ' + stdout);    
-			console.log('stderr: ' + stderr)       
-	} else {
-	 	console.log(err);
-		console.log(err.code);           
-		console.log(err.signal);	
-	}           
-})
+
+		 // Sleep
+		 var child = exec('pmset displaysleepnow', function(err, stdout, stderr) {
+        
+		 })
 });
 
